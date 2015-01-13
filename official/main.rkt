293ee4ee349f008a9b2c74074294f2e5930b1fbd
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-01-13 14:17:41 -0500

    Fix relative path calculation in main.rkt

--- official/main.rkt
+++ official/main.rkt
@@ -4,6 +4,9 @@
 
 (require "config.rkt")
 
+(require racket/runtime-path)
+(define-runtime-path here ".")
+
 (define (main [configuration (hash)])
   (config configuration)
-  ((dynamic-require "dynamic.rkt" 'go)))
+  ((dynamic-require (build-path here "dynamic.rkt") 'go)))
