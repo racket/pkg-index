81538aabe394242d48335c95fbd125476d4ef803
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2015-01-26 18:02:45 -0500

    Create target directory before rsyncing to it; also, rsync to a different place

--- official/configs/private/tonyg-s3cmd.py
+++ official/configs/private/tonyg-s3cmd.py
@@ -4,5 +4,6 @@ import sys
 import os
 
 print 's3cmd-placeholder:', sys.argv
-os.system('rsync -av --delete %s ~/public_html/pkg-catalog-static' % \
+os.system('mkdir -p ~/public_html/pkg-index-static')
+os.system('rsync -av --delete %s ~/public_html/pkg-index-static' % \
           (sys.argv[-2],))
