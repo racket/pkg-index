9920e297014c357dd9f4ad6eb882a7d78ecb2017
Jay McCarthy <jay.mccarthy@gmail.com>
2019-01-19 13:33:13 +0000

    paths

--- planet-compat/s3.sh
+++ planet-compat/s3.sh
@@ -3,7 +3,7 @@ set -e
 
 . ~/.profile
 
-export PC_ROOT="${PC_ROOT:-~/Dev/local/pkg.rlo/planet-compat/root/}"
+export PC_ROOT="${PC_ROOT:-~/Dev/scm/plt/extra-pkgs/pkg-index/planet-compat}"
 export S3CFG_PLT="${S3CFG_PLT:-~/.s3cfg-plt}"
 export S3_BUCKET="${S3_BUCKET:-planet-compats.racket-lang.org}"
 export S3CMD="${S3CMD:-s3cmd}"
